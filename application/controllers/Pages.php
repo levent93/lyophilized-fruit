d692972ad9ce41720f1829cec2dff79e19bb7325
MrX93 <nikola.milicevic@protonmail.com>
2018-01-10 16:14:12 +0100

    Some settings
    Config files and default contoller.

---
+++ application/controllers/Pages.php
@@ -0,0 +1,25 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of pocetna
+ *
+ * @author Ultimate
+ */
+class Pages extends MY_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+    public function index(){
+    	
+          echo 'Hello World!';
+    }
+
+}
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of MY_Controller
+ *
+ * @author Ultimate
+ */
+class MY_Controller extends CI_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
+
+	public function page($page_name, $data = array()) {
+		
+	}
+
+}
